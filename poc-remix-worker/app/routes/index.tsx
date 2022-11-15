5fc6f9ff53ab543b9cf0e0b88d5c777f8cee7e0a
drifterz13 <mixmyxxe@gmail.com>
2022-11-15 20:25:50 +0700

    prettier

--- poc-remix-worker/app/routes/index.tsx
+++ poc-remix-worker/app/routes/index.tsx
@@ -1,3 +1,15 @@
+import { Link } from '@remix-run/react'
+
 export default function Index() {
- return <h1 className='text-2xl'>Home page</h1>
+  return (
+    <div className="px-20 py-10">
+      <h1 className="text-2xl">Home page</h1>
+      <Link
+        to="/projects"
+        className="cursor-pointer hover:text-blue-600 focus:ring-2 active:text-blue-400"
+      >
+        Projects
+      </Link>
+    </div>
+  )
 }
--- poc-remix-worker/prisma/seed.ts
+++ poc-remix-worker/prisma/seed.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = 'rachel@remix.run'
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
-  });
+  })
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash('racheliscool', 10)
 
   const user = await prisma.user.create({
     data: {
@@ -22,16 +22,16 @@ async function seed() {
         },
       },
     },
-  });
+  })
 
-  console.log(`Database has been seeded. 🌱`);
+  console.log(`Database has been seeded. 🌱`)
 }
 
 seed()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
